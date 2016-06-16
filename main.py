9084bb8b4da2b715011928f84023deb55053cb3e
Bucky <buckyroberts@gmail.com>
2016-06-16 04:03:20 -0400

    Updated samples

--- main.py
+++ main.py
@@ -5,26 +5,26 @@ API = thenewbostonApi(IP_ADDRESS, PORT, API_TOKEN)
 
 
 def adding_data_demo():
-    API.add_major('adminToken', 'Physics', 'physics')
-    API.add_video('adminToken', 2, 3, 'Some Random Title', 'KQzxkeH3ebE')
-    API.add_course('computer-science', 'adminToken', 'Test Category', 'Test Course', 3)
+    API.add_major('adminToken', 'Apples', 'apples')
+    API.add_video('adminToken', 2, 3, 'Title of Video', 'KQzxkeH3ebE')
+    API.add_course('computer-science', 'adminToken', 'Course Category', 'Course Name', 3)
 
 
 def getting_data_demo():
 
     print('\n----- All Majors -----')
     for major in API.get_majors_or_course('majors'):
-        print(major['name'])
+        print(major)
 
     print('\n----- Courses -----')
     for course in API.get_majors_or_course('computer-science'):
-        print(course['name'])
+        print(course)
 
     print('\n----- Videos -----')
     for video in API.get_videos(1):
-        print(video['title'])
+        print(video)
 
 
 if __name__ == '__main__':
-    adding_data_demo()
+    # adding_data_demo()
     getting_data_demo()
